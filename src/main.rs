1f5e32c12c5e980fe1bf991889e7156cab29243e
Arnas Milaševičius <arnasmila@gmail.com>
2024-10-07 15:08:36 -0500

    added minimal route handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 use std::net::TcpListener;
 
 fn main() {
@@ -7,7 +7,19 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                _stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
+                let buf_reader = BufReader::new(&mut _stream);
+                let request_line = buf_reader.lines().next();
+                let response_line = match request_line {
+                    Some(line) => {
+                        match line.unwrap().as_str() {
+                            "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
+                            _ => "HTTP/1.1 404 Not Found\r\n\r\n"
+                        }
+                    },
+                    None => "HTTP/1.1 404 Not Found\r\n\r\n",
+                };
+
+                _stream.write_all(response_line.as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
