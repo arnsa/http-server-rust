838759a70bb050f3a2448075e96dcb21fed81cce
Arnas Milaševičius <arnasmila@gmail.com>
2024-10-09 14:03:43 -0500

    compress body with gzip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use std::{env, fs};
+use flate2::write::GzEncoder;
+use flate2::Compression;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::str;
 use std::thread;
+use std::{env, fs};
 
 fn main() {
     let mut threads = Vec::new();
@@ -37,8 +40,8 @@ fn handle_connection(mut stream: TcpStream) {
 
     let request_line = request[0];
 
-    let response_line = match request_line {
-        "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
+    let (response_line, data) = match request_line {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK\r\n\r\n".to_string(), None),
         "GET /user-agent HTTP/1.1" => {
             let user_agent = request.iter().find(|line| line.contains("User-Agent:"));
 
@@ -49,24 +52,34 @@ fn handle_connection(mut stream: TcpStream) {
             let response = user_agent.unwrap().replace("User-Agent: ", "");
             let length = response.len();
 
-            format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{response}")
-        },
+            (format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{response}"), None)
+        }
         line if line.starts_with("GET /echo/") && line.ends_with("HTTP/1.1") => {
             let echo_str = parse_file_name_from_url(&line, "GET /echo/");
             let accept_encoding = request.iter().find(|x| x.contains("Accept-Encoding"));
-            let length = echo_str.len();
-            let mut content_encoding = String::new();
+            let mut length = echo_str.len();
+            let mut content_encoding = String::from("\r\n");
 
-            if let Some(encoding) = accept_encoding {
-                let encoding = encoding.split("Accept-Encoding: ").nth(1).unwrap();
+            match accept_encoding {
+                Some(encoding) => {
+                    let encoding = encoding.split("Accept-Encoding: ").nth(1).unwrap();
 
-                if encoding.contains("gzip") {
-                    content_encoding = format!("\r\nContent-Encoding: gzip\r\n");
+                    if encoding.contains("gzip") {
+                        let mut encoder = GzEncoder::new(vec![], Compression::default());
+                        let _ = encoder.write_all(&echo_str.as_bytes());
+                        let compressed_data = encoder.finish().unwrap();
+
+                        length = compressed_data.len();
+                        content_encoding = format!("\r\nContent-Encoding: gzip\r\n");
+
+                        (format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain{content_encoding}Content-Length: {length}\r\n\r\n"), Some(compressed_data))
+                    } else {
+                        (format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain{content_encoding}Content-Length: {length}\r\n\r\n{echo_str}"), None)
+                    }
                 }
-            };
-
-            format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain{content_encoding}\r\nContent-Length: {length}\r\n\r\n{echo_str}")
-        },
+                None => (format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain{content_encoding}Content-Length: {length}\r\n\r\n{echo_str}"), None),
+            }
+        }
         line if line.starts_with("GET /files/") && line.ends_with("HTTP/1.1") => {
             let file_name = parse_file_name_from_url(&line, "GET /files/");
             let directory = parse_directory_from_args();
@@ -76,9 +89,12 @@ fn handle_connection(mut stream: TcpStream) {
                 Ok(contents) => {
                     let length = contents.len();
 
-                    format!("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {length}\r\n\r\n{contents}")
-                },
-                Err(_) => "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+                    (
+                        format!("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {length}\r\n\r\n{contents}"),
+                        None
+                    )
+                }
+                Err(_) => ("HTTP/1.1 404 Not Found\r\n\r\n".to_string(), None),
             }
         }
         line if line.starts_with("POST /files/") && line.ends_with("HTTP/1.1") => {
@@ -87,28 +103,40 @@ fn handle_connection(mut stream: TcpStream) {
             let content_length = request.iter().find(|x| x.contains("Content-Length"));
 
             if content_length.is_none() {
-                "HTTP/1.1 500 An Error Ocurred\r\n\r\n".to_string()
+                ("HTTP/1.1 500 An Error Ocurred\r\n\r\n".to_string(), None)
             } else {
-                let content_length = content_length.unwrap().split("Content-Length: ").nth(1).unwrap().parse::<usize>().unwrap();
+                let content_length = content_length
+                    .unwrap()
+                    .split("Content-Length: ")
+                    .nth(1)
+                    .unwrap()
+                    .parse::<usize>()
+                    .unwrap();
                 let body = &request[request.len() - 1][0..content_length];
 
                 if let Err(_) = fs::write(format!("{}{}", directory, file_name), body) {
-                    "HTTP/1.1 500 An Error Ocurred\r\n\r\n".to_string()
+                    ("HTTP/1.1 500 An Error Ocurred\r\n\r\n".to_string(), None)
                 } else {
-                    "HTTP/1.1 201 Created\r\n\r\n".to_string()
+                    ("HTTP/1.1 201 Created\r\n\r\n".to_string(), None)
                 }
             }
         }
-        _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+        _ => ("HTTP/1.1 404 Not Found\r\n\r\n".to_string(), None),
     };
 
     stream.write_all(response_line.as_bytes()).unwrap();
+
+    if let Some(d) = data {
+        stream.write_all(&d).unwrap();
+    }
 }
 
 fn parse_directory_from_args() -> String {
     let args: Vec<String> = env::args().collect();
     let mut directory = String::from("./");
-    let directory_arg_idx = args.iter().position(|arg| arg == "directory" || arg == "--directory");
+    let directory_arg_idx = args
+        .iter()
+        .position(|arg| arg == "directory" || arg == "--directory");
 
     if directory_arg_idx.is_some() && directory_arg_idx.unwrap() < args.len() {
         directory = String::from(&args[directory_arg_idx.unwrap() + 1]);
