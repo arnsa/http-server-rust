1a0029870df7d4146a839a2a88a53df38acb8d23
Arnas Milaševičius <arnasmila@gmail.com>
2024-10-21 16:08:24 -0500

    added unit tests for response module

--- server/src/response/builder.rs
+++ server/src/response/builder.rs
@@ -37,3 +37,64 @@ impl Display for Response {
         )
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::http::{code::HttpCode, header::HttpHeader};
+
+    #[test]
+    fn test_display_response_with_headers_and_body() {
+        let response = Response {
+            status_code: HttpCode::Ok,
+            status_text: "OK".to_string(),
+            http_version: "HTTP/1.1".to_string(),
+            headers: Some(vec![
+                HttpHeader::ContentType("application/json".to_string()),
+                HttpHeader::ContentEncoding("deflate".to_string()),
+                HttpHeader::ContentLength(27),
+            ]),
+            body: Some("{\"message\":\"Success\"}".to_string()),
+        };
+
+        let expected = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Encoding: deflate\r\nContent-Length: 27\r\n\r\n{\"message\":\"Success\"}";
+        let formatted = format!("{}", response);
+
+        assert_eq!(formatted, expected);
+    }
+
+    #[test]
+    fn test_display_response_without_headers() {
+        let response = Response {
+            status_code: HttpCode::NotFound,
+            status_text: "Not Found".to_string(),
+            http_version: "HTTP/1.0".to_string(),
+            headers: None,
+            body: Some("The requested resource was not found.".to_string()),
+        };
+
+        let expected = "HTTP/1.0 404 Not Found\r\n\r\nThe requested resource was not found.";
+        let formatted = format!("{}", response);
+
+        assert_eq!(formatted, expected);
+    }
+
+    #[test]
+    fn test_display_response_without_body() {
+        let response = Response {
+            status_code: HttpCode::NoContent,
+            status_text: "No Content".to_string(),
+            http_version: "HTTP/1.1".to_string(),
+            headers: Some(vec![
+                HttpHeader::ContentType("text/plain".to_string()),
+                HttpHeader::ContentEncoding("gzip".to_string()),
+            ]),
+            body: None,
+        };
+
+        let expected = "HTTP/1.1 204 No Content\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\n\r\n";
+        let formatted = format!("{}", response);
+
+        assert_eq!(formatted, expected);
+    }
+}
