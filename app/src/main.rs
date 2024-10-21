cd7b8ab897265cbe2223e7660c1759eff41f888a
Arnas Milaševičius <arnasmila@gmail.com>
2024-10-21 16:58:06 -0500

    added thread pool implementation

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,28 +1,25 @@
 mod router;
 mod utils;
 
-use std::{net::TcpListener, thread};
+use std::net::TcpListener;
 
+use server::thread_pool::ThreadPool;
 use utils::handle_tcp_connection;
 
 fn main() {
-    let mut threads = Vec::new();
+    let pool = ThreadPool::new(4);
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                threads.push(thread::spawn(|| {
+                pool.execute(|| {
                     let _ = handle_tcp_connection(_stream);
-                }));
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
-    }
-
-    for t in threads {
-        let _ = t.join();
     }
 }
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -2,3 +2,4 @@ pub mod http;
 pub mod request;
 pub mod response;
 pub mod url;
+pub mod thread_pool;
